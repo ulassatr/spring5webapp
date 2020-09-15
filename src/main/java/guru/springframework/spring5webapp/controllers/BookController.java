f2aa20809c44ab2ecb03bc8b13fc07177b051619
ulassatr <satirulas@gmail.com>
2020-09-15 09:34:13 +0300

    thymeleaf

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BookController {
@@ -18,7 +21,8 @@ public class BookController {
     public String getBooks(Model model){
 
         model.addAttribute("books",bookRepository.findAll());
-
+        List<Book> bookList=  (List<Book>) model.getAttribute("books");
+        bookList.forEach(x-> System.out.println(x));
         return "books/list";
     }
 }
